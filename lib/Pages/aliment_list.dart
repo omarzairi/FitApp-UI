be26daa805c00bf13f24a0f8d1a6806d65a703a8
omar zairi <zairiomar044@gmail.com>
2023-11-11 19:50:40 +0100

    got Aliments

--- lib/Pages/aliment_list.dart
+++ lib/Pages/aliment_list.dart
@@ -1,4 +1,22 @@
+import 'dart:convert';
+
+import 'package:fitapp/classes/Aliment.dart';
 import 'package:flutter/material.dart';
+
+import 'package:http/http.dart' as http;
+
+
+Future<List<Aliment>> fetchAliments() async {
+  final response =
+      await http.get(Uri.parse('https://fit-app-api.azurewebsites.net/api/aliments'));
+
+  if (response.statusCode == 200) {
+    List<dynamic> body = jsonDecode(response.body);
+    return body.map((dynamic item) => Aliment.fromJson(item)).toList();
+  } else {
+    throw Exception('Failed to load aliments');
+  }
+}
 
 class AlimentListPage extends StatefulWidget {
   @override
@@ -6,7 +24,13 @@ class AlimentListPage extends StatefulWidget {
 }
 
 class _AlimentListPageState extends State<AlimentListPage> {
-  final List<String> aliments = ['Aliment 1', 'Aliment 2', 'Aliment 3']; // Add your aliments here
+  late Future<List<Aliment>> futureAliments;
+
+  @override
+  void initState() {
+    super.initState();
+    futureAliments = fetchAliments() as Future<List<Aliment>>;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,12 +38,24 @@ class _AlimentListPageState extends State<AlimentListPage> {
       appBar: AppBar(
         title: Text('Aliment List'),
       ),
-      body: ListView.builder(
-        itemCount: aliments.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(aliments[index]),
-          );
+      body: FutureBuilder<List<Aliment>>(
+        future: futureAliments,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+              itemCount: snapshot.data?.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(snapshot.data![index].name),
+                );
+              },
+            );
+          } else if (snapshot.hasError) {
+            return Text("${snapshot.error}");
+          }
+
+          // By default, show a loading spinner.
+          return CircularProgressIndicator();
         },
       ),
     );
