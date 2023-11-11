f1da3d3fc9437e3b4279e50d565d6380e7e3575f
omar zairi <zairiomar044@gmail.com>
2023-11-11 20:51:22 +0100

    finished first version of aliment list

--- lib/Pages/aliment_list.dart
+++ lib/Pages/aliment_list.dart
@@ -2,13 +2,11 @@ import 'dart:convert';
 
 import 'package:fitapp/classes/Aliment.dart';
 import 'package:flutter/material.dart';
-
 import 'package:http/http.dart' as http;
 
-
 Future<List<Aliment>> fetchAliments() async {
-  final response =
-      await http.get(Uri.parse('https://fit-app-api.azurewebsites.net/api/aliments'));
+  final response = await http
+      .get(Uri.parse('https://fit-app-api.azurewebsites.net/api/aliments'));
 
   if (response.statusCode == 200) {
     List<dynamic> body = jsonDecode(response.body);
@@ -25,28 +23,86 @@ class AlimentListPage extends StatefulWidget {
 
 class _AlimentListPageState extends State<AlimentListPage> {
   late Future<List<Aliment>> futureAliments;
+  late List<bool> selectedAliments;
 
   @override
   void initState() {
     super.initState();
-    futureAliments = fetchAliments() as Future<List<Aliment>>;
+    futureAliments = fetchAliments();
+    selectedAliments = [];
+  }
+
+  String formatNumber(double num) {
+    return num % 1 == 0 ? num.toInt().toString() : num.toString();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Aliment List'),
-      ),
       body: FutureBuilder<List<Aliment>>(
         future: futureAliments,
         builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done &&
+              selectedAliments.isEmpty) {
+            selectedAliments = List<bool>.filled(snapshot.data!.length, false);
+          }
           if (snapshot.hasData) {
             return ListView.builder(
               itemCount: snapshot.data?.length,
               itemBuilder: (context, index) {
-                return ListTile(
-                  title: Text(snapshot.data![index].name),
+                return Card(
+                  child: Row(
+                    children: <Widget>[
+                      Checkbox(
+                        value: selectedAliments[index],
+                        onChanged: (bool? value) {
+                          setState(() {
+                            selectedAliments[index] = value!;
+                          });
+                        },
+                      ),
+                      ClipOval(
+                        child: Image.network(
+                          snapshot.data![index].image!,
+                          width: 50,
+                          height: 50,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              snapshot.data![index].name,
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Row(
+                              children: <Widget>[
+                                Text(
+                                  '${formatNumber(snapshot.data![index].calories)}',
+                                  style: TextStyle(
+                                    color: Colors.blueAccent,
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                Text(
+                                  ' / ${formatNumber(snapshot.data![index].servingSize)} ${snapshot.data![index].servingUnit}',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
                 );
               },
             );
@@ -58,6 +114,15 @@ class _AlimentListPageState extends State<AlimentListPage> {
           return CircularProgressIndicator();
         },
       ),
+      floatingActionButton: selectedAliments.contains(true)
+          ? FloatingActionButton.extended(
+              onPressed: () {
+                // Add your action here
+              },
+              label: Text('Add (${selectedAliments.where((b) => b).length})'),
+              icon: Icon(Icons.check),
+            )
+          : null,
     );
   }
 }
