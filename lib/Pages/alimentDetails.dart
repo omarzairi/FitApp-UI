04dc37aafbbce24dd058e583af002447606ae67d
YosraSaadani <yosrasaadani19@gmail.com>
2023-11-12 16:58:48 +0100

    merge details and list

--- lib/Pages/alimentDetails.dart
+++ lib/Pages/alimentDetails.dart
@@ -4,16 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:fl_chart/fl_chart.dart';
 
-Future<Aliment> fetchAliment() async {
-  final response = await http.get(Uri.parse('https://fit-app-api.azurewebsites.net/api/aliments'));
-
-  if (response.statusCode == 200) {
-    dynamic body = jsonDecode(response.body)[0];
-    return Aliment.fromJson(body);
-  } else {
-    throw Exception('Failed to load aliment');
-  }
-}
+import '../utils/theme_colors.dart';
 
 class PieChartSample2 extends StatelessWidget {
   final double calories ;
@@ -129,118 +120,157 @@ class Indicator extends StatelessWidget {
   }
 }
 
+
 class AlimentDetails extends StatefulWidget {
   final Aliment aliment;
+
+  const AlimentDetails({Key? key, required this.aliment}) : super(key: key);
+
   @override
   _AlimentDetailsState createState() => _AlimentDetailsState();
-  const AlimentDetails({super.key, required this.aliment});
-
 }
 
 class _AlimentDetailsState extends State<AlimentDetails> {
-  late Future<Aliment> futureAliment;
+  late Aliment _aliment;
 
   @override
   void initState() {
     super.initState();
-    futureAliment = fetchAliment();
+    _aliment = widget.aliment;
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: TColor.white,
+        centerTitle: true,
+        elevation: 0,
+        leading: InkWell(
+          onTap: () {
+            Navigator.pop(context);
+          },
+          child: Container(
+            margin: const EdgeInsets.all(8),
+            height: 40,
+            width: 40,
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+                color: TColor.lightGray,
+                borderRadius: BorderRadius.circular(10)),
+            child: Icon(
+              Icons.arrow_back_ios_new,
+              color: TColor.black,
+              size: 20,
+            ),
+          ),
+        ),
+        title: Text(_aliment.name,
+            style: TextStyle(
+              color: TColor.black,
+              fontSize: 20,
+              letterSpacing: 1,
+              fontWeight: FontWeight.w500,
+            )),
+        actions: [
+          InkWell(
+            onTap: () {},
+            child: Container(
+              margin: const EdgeInsets.all(8),
+              height: 40,
+              width: 40,
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                  color: TColor.lightGray,
+                  borderRadius: BorderRadius.circular(10)),
+              child: Icon(
+                Icons.more_horiz,
+                color: TColor.black,
+                size: 20,
+              ),
+            ),
+          )
+        ],
+      ),
+        body: SingleChildScrollView(
+        child: Padding(
+        padding: const EdgeInsets.all(16.0),
+    child: Column(
 
-      body: FutureBuilder<Aliment>(
-        future: futureAliment,
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            final aliment = snapshot.data!;
-            return Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
+
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              width: double.infinity,
+              child: Image(
+                image: NetworkImage(_aliment.image ?? ''),
+                fit: BoxFit.contain,
+              ),
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: NetworkImage(_aliment.image ?? ''),
+                  fit: BoxFit.cover,
+                ),
+                borderRadius: BorderRadius.circular(12.0),
+              ),
+            ),
+            const SizedBox(height: 16),
+
+
+                Text(
+                  _aliment.name,
+                  style: const TextStyle(
+
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 1,
+                    color: Colors.black,
+                    fontFamily: 'Poppins',
+                    fontFamilyFallback: <String>[
+                      'Noto Sans CJK SC',
+                      'Noto Color Emoji',
+                    ],
+                  ),
+
+
+            ),
+            const SizedBox(height: 40),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Container(
-                    width: double.infinity,
-                    height: 200,
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: NetworkImage(aliment.image ?? ''),
-                        fit: BoxFit.cover,
-                      ),
-                      borderRadius: BorderRadius.circular(12.0),
-                    ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      _buildDetailItem('Calories', _aliment.calories.toString()),
+                      _buildDetailItem('Protein', _aliment.protein.toString()),
+                    ],
                   ),
-                 const SizedBox(height: 16),
-                  Row( children: [
-                 const    Text(
-                      '   Name :     ',
-                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold,color: Colors.deepPurple,
-                        fontFamily: 'ubuntu', fontFamilyFallback: <String>[
-                          'Noto Sans CJK SC',
-                          'Noto Color Emoji',
-                        ],),
-                    ),
-                    Text(
-                      aliment.name,
-                      style: const TextStyle(fontSize: 20, fontWeight: FontWeight.normal,color: Colors.black,
-                        fontFamily: 'ubuntu', fontFamilyFallback: <String>[
-                          'Noto Sans CJK SC',
-                          'Noto Color Emoji',
-                        ],),
-                    ),
-                  ],),
-
-                  const SizedBox(height: 40),
-                  Container(
-                    margin:const EdgeInsets.symmetric(horizontal: 16.0), // Adjust the horizontal margin as needed
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            _buildDetailItem(
-                              'Calories',
-                              aliment.calories.toString(),
-                            ),
-                            _buildDetailItem('Protein', aliment.protein.toString()),
-                          ],
-                        ),
-                        const SizedBox(width: 8), // Add spacing between the two columns
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            _buildDetailItem('Carbs', aliment.carbs.toString()),
-                            _buildDetailItem('Fat', aliment.fat.toString()),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-
-
-                 const SizedBox(height: 50),
-                  PieChartSample2(
-                    calories: aliment.calories,
-                    protein: aliment.protein,
-                    carbs: aliment.carbs,
-                    fat: aliment.fat,
-                    sugar: aliment.sugar,
+                  const SizedBox(width: 8),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      _buildDetailItem('Carbs', _aliment.carbs.toString()),
+                      _buildDetailItem('Fat', _aliment.fat.toString()),
+                    ],
                   ),
                 ],
               ),
-            );
-          } else if (snapshot.hasError) {
-            return Center(child: Text("${snapshot.error}"));
-          }
-
-          // By default, show a loading spinner.
-          return const Center(child: CircularProgressIndicator());
-        },
+            ),
+            const SizedBox(height: 40),
+            PieChartSample2(
+              calories: _aliment.calories,
+              protein: _aliment.protein,
+              carbs: _aliment.carbs,
+              fat: _aliment.fat,
+              sugar: _aliment.sugar,
+            ),
+          ],
+        ),)
       ),
     );
   }
